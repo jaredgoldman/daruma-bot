d94867b70aa468163ab4b4b55bc7c636cce10eae
jaredgoldman <jaredgoldmandrums@gmail.com>
2022-08-02 21:53:01 -0400

    build: create game model

--- dist/models/game.js
+++ dist/models/game.js
@@ -1,6 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 class Game {
-    constructor() { }
+    constructor(players, active, win, megatron, npc, embed, waitingRoom, stopped, update) {
+        this.players = players;
+        this.active = active;
+        this.win = win;
+        this.megatron = megatron;
+        this.npc = npc;
+        this.embed = embed;
+        this.waitingRoom = waitingRoom;
+        this.stopped = stopped;
+        this.update = update;
+    }
 }
 exports.default = Game;
--- src/models/game.ts
+++ src/models/game.ts
@@ -1,3 +1,19 @@
+import Player from './player'
 export default class Game {
-  constructor() {}
+  constructor(
+    public players: { [key: string]: Player },
+    public active: boolean,
+    public win: false,
+    public megatron?: any,
+    public npc?: NPC,
+    public embed?: any,
+    public waitingRoom?: any,
+    public stopped?: boolean,
+    public update?: boolean
+  ) {}
+}
+
+interface NPC {
+  hp: number
+  dead: boolean
 }
