4f164f4d43d92b58e68fb79486213a533126925e
jaredgoldman <jaredgoldmandrums@gmail.com>
2022-08-02 21:53:20 -0400

    build: create user model

--- dist/models/user.js
+++ dist/models/user.js
@@ -1,8 +1,20 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 class User {
-    constructor(discordId) {
+    constructor(username, discordId, address, assets, karma, losses, 
+    //key: EnhancerType
+    enhancers, wins, _id, coolDowns // timestamp
+    ) {
+        this.username = username;
         this.discordId = discordId;
+        this.address = address;
+        this.assets = assets;
+        this.karma = karma;
+        this.losses = losses;
+        this.enhancers = enhancers;
+        this.wins = wins;
+        this._id = _id;
+        this.coolDowns = coolDowns;
     }
 }
 exports.default = User;
--- src/models/user.ts
+++ src/models/user.ts
@@ -1,3 +1,33 @@
+import { ObjectId } from 'mongodb'
+
 export default class User {
-  constructor(public discordId: string) {}
+  constructor(
+    public username: string,
+    public discordId: string,
+    public address: string,
+    public assets: {
+      [key: string]: UserAsset
+    },
+    public karma: number,
+    public losses: number,
+    //key: EnhancerType
+    public enhancers: { [key: string]: Enhancer },
+    public wins?: { [key: string]: number },
+    public _id?: ObjectId,
+    public coolDowns?: { [key: string]: number } // timestamp
+  ) {}
 }
+
+export interface UserAsset {
+  alias?: string
+  wins: number
+  losses: number
+  kos: number
+}
+
+export interface Enhancer {
+  type: EnancerType
+  owned: boolean
+}
+
+type EnancerType = 'arms' | 'legs' | 'meditation'
