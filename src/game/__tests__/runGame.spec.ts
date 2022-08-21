ad9fb207548cdfc360737296f669e24fa38a6b7d
jaredgoldman <jaredgoldmandrums@gmail.com>
2022-08-21 09:30:06 -0400

    build: delete example test - move runGame in test outer test suite scope

--- src/game/__tests__/runGame.spec.ts
+++ src/game/__tests__/runGame.spec.ts
@@ -3,19 +3,15 @@ import { players } from '../../mocks/game'
 import Game from '../../models/game'
 
 describe('game test suite', () => {
-  //@ts-ignore
   const game = new Game(players, false, false, 2, '1005510628217192579')
 
+  runGame(game, true)
+
   it('game should produce a winner with 21 as total score', async () => {
-    await runGame(game, true)
     expect(game.win).toBe(true)
     expect(
       game.winnerDiscordId && players[game.winnerDiscordId].totalScore === 21
     ).toBeTruthy()
-  })
-
-  it('game should not be active after return', async () => {
-    await runGame(game, true)
     expect(game.active).toBe(false)
   })
 })
