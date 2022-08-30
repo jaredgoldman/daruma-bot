e618daee31f50eafacadb6736042fc3591dff15d
Scott <2660603+ShoGinn@users.noreply.github.com>
2022-08-29 20:31:05 -0400

    build: remove built ts files

--- dist/constants/embeds.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Embeds = void 0;
-var Embeds;
-(function (Embeds) {
-    Embeds[Embeds["waitingRoom"] = 0] = "waitingRoom";
-})(Embeds = exports.Embeds || (exports.Embeds = {}));
