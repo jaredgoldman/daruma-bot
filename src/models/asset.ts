6d8e8527c5c48c73b1092249389f8ab7b6da0753
jaredgoldman <jaredgoldmandrums@gmail.com>
2022-09-05 12:16:34 -0400

    improve asset class

--- src/models/asset.ts
+++ src/models/asset.ts
@@ -3,8 +3,27 @@ export default class Asset {
     public assetId: number,
     public assetName: string,
     public assetUrl: string,
-    public unitName: string,
-    public localPath?: string,
-    public alias?: string
-  ) {}
+    public unitName: string
+  ) {
+    this.alias = undefined
+    this.localPath = undefined
+  }
+
+  private alias: string | undefined
+  setAlias(alias: string) {
+    this.alias = alias
+  }
+
+  getAlias() {
+    return this.alias
+  }
+
+  private localPath: string | undefined
+  setLocalPath(localPath: string) {
+    this.localPath = localPath
+  }
+
+  getLocalPath() {
+    return this.localPath
+  }
 }
