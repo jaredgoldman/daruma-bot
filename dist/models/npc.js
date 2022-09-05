1ee498cef26a13921574823997e384d954bd8d7d
jaredgoldman <jaredgoldmandrums@gmail.com>
2022-09-05 15:22:07 -0400

    untrack js files

--- dist/models/npc.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class NPC {
-    constructor(hp, dead) {
-        this.hp = hp;
-        this.dead = dead;
-    }
-}
-exports.default = NPC;
